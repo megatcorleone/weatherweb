18add67d7116d5a87b44993eb8d1302c3d216f30
localadmin <localadmin@localadmins-MacBook-Pro-3.local>
2018-08-24 19:20:13 +0800

    Almost Done

--- Server.js
+++ Server.js
@@ -12,12 +12,37 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  var weather
+  res.render('index', {
+    weather: null
+  });
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+// app.get('/', function (req, res) {
+//
+//     let city = req.body.city;
+//     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+//
+//   request(url, function (err, response, body) {
+//       console.log(body)
+//       if(err){
+//         res.render('index', {weather: null, error: 'Error, please try again'});
+//       } else {
+//         let weather = JSON.parse(body)
+//         console.log(123)
+//         if(weather.main == undefined){
+//           res.render('index', {weather: null, error: 'Error, please try again'});
+//         } else {
+//           let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+//           res.render('index', {weather: weatherText, error: null});
+//         }
+//       }
+//     });
+// });
 
 app.post('/', function (req, res) {
 
@@ -25,10 +50,12 @@ app.post('/', function (req, res) {
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
   request(url, function (err, response, body) {
+      console.log(body)
       if(err){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
         let weather = JSON.parse(body)
+        console.log(123)
         if(weather.main == undefined){
           res.render('index', {weather: null, error: 'Error, please try again'});
         } else {
@@ -37,4 +64,4 @@ app.post('/', function (req, res) {
         }
       }
     });
-})
+});
