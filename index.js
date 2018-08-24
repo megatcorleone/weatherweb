a018ec6f8ce747e89ac7e98b9b623b27feaf6c5b
localadmin <localadmin@localadmins-MacBook-Pro-3.local>
2018-08-24 11:43:46 +0800

    console done

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = '6f7421152f141c7c74a439b6b1078c61';
+let city = argv.c || 'kuala lumpur';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
     console.log('body:', body);
+
+    let weather = JSON.parse(body)
+
+    let message = `It's ${weather.main.temp} degrees in
+                   ${weather.name}!`;
+    console.log(message);
   }
+
+
 });
